d87d5a845a2cba4c0fc970bb159b1df253c6b220
Blake Tucker <Blake.Tucker@aexp.com>
2016-04-08 15:36:32 -0700

    Initial Commit

---
+++ PetFinderTests/PetFinderTests.swift
@@ -0,0 +1,36 @@
+//
+//  PetFinderTests.swift
+//  PetFinderTests
+//
+//  Created by Blake Tucker on 4/8/16.
+//  Copyright © 2016 Blake Tucker. All rights reserved.
+//
+
+import XCTest
+@testable import PetFinder
+
+class PetFinderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
